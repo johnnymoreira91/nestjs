8c0c6324169bfab6a1fb58badcde7302ae8d64b0
Johnny Moreira <johnnymoreira91@hotmail.com>
2023-06-18 17:39:11 -0300

    add swagger

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -6,31 +6,38 @@ import {
   Matches,
 } from 'class-validator';
 import { CreateUser } from 'src/models/users';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto implements CreateUser {
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty()
   age: number;
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty()
   roleId: number;
 
   @IsNotEmpty()
   @IsBoolean()
+  @ApiProperty()
   superUser: boolean;
 
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsNotEmpty()
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message: 'password too weak',
   })
+  @ApiProperty()
   password;
 
   // @IsEmpty()
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,19 +7,40 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import {
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
+@ApiTags('Users')
 @Controller('api/v1/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({ summary: 'Create User' })
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'The body is missing something.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  @ApiConsumes('application/json')
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'The found record',
+    type: User,
+  })
   @Get()
   findAll() {
     return this.userService.findAll();
